b1a3be0db8d1e917eaaa4aeb6c3659fcb0bb1727
Pietro Martins de Oliveira <pietromartinso@gmail.com>
2023-02-06 18:53:20 -0300

    vector allocation with new operator test

--- poo-disciplina/main.cpp
+++ poo-disciplina/main.cpp
@@ -6,22 +6,11 @@
 #include "Disciplina.hpp"
 
 int main(){
-	Pessoa* ptr1{ new Pessoa };//Utilizando construtor default
-	Pessoa* ptr2{ new Pessoa{"Joana", 22} };//Utilizando construtor com parâmetros
-
-	int* ptrInt{ new int };//inteiro alocado e com lixo de memória
-	int* ptrIntIniciado{ new int{2} };//inteiro alocado e inicializado com 2
-
-	ptr1->setNome("Maria");//operador -> para derreferenciar a função de objeto apontado
-	*ptrInt = 20;//mesma coisa que com C
-
-	std::cout << ptr1->getNome() << std::endl;
-	std::cout << ptr2->getNome() << std::endl;
-	std::cout << *ptrInt << std::endl;
-	std::cout << *ptrIntIniciado << std::endl;
+	int* array{ new int[10] };//vetor de 10 posições
+	for (int i = 0; i < 10; i++) {
+		array[i] = i;
+		std::cout << array[i] << std::endl;
+	}
 	return 0;
 
-
-
-	return 0;
 }
