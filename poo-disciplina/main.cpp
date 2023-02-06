c56c54db98ce137aa190769434aea433cf14ea9b
Pietro Martins de Oliveira <pietromartinso@gmail.com>
2023-02-06 19:24:11 -0300

    adding delete operator

--- poo-disciplina/main.cpp
+++ poo-disciplina/main.cpp
@@ -6,11 +6,22 @@
 #include "Disciplina.hpp"
 
 int main(){
-	int* array{ new int[10] };//vetor de 10 posições
+
+	int** mat = new int*[10];
+
+	int k = 1;
+
 	for (int i = 0; i < 10; i++) {
-		array[i] = i;
-		std::cout << array[i] << std::endl;
+		mat[i] = new int[10];
+		for (int j = 0; j < 10; j++) {
+			mat[i][j] = k;
+			k++;
+			std::cout << mat[i][j] << "\t";
+		}
+		std::cout << std::endl;
 	}
+
+	delete[] mat;
 	return 0;
 
 }
